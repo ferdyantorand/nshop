8161845b4a972eb500fd7b0400412edf21d4d8a2
yansen626 <yansen626@gmail.com>
2019-07-23 16:34:23 +0700

    update 23 juli - emoji 5 karakter

--- resources/views/frontend/products/show.blade.php
+++ resources/views/frontend/products/show.blade.php
@@ -141,9 +141,9 @@
                                         <input type="hidden" id="src-list" value="">
 
                                         <div id="emoji_list" class="field radio_field" style="display: none;padding-bottom: 5%;">
-                                            <div class="col-xs-12 col-sm-12 col-md-12 text--right" style="padding-bottom: 5%;">
-                                                *Please hit refresh for emoji visualization
-                                            </div>
+                                            {{--<div class="col-xs-12 col-sm-12 col-md-12 text--right" style="padding-bottom: 5%;">--}}
+                                                {{--*Please hit refresh for emoji visualization--}}
+                                            {{--</div>--}}
                                             <label style="padding-right: 3%;cursor: pointer;">
                                                 <input type="radio" name="emoji"
                                                        onchange="SelectEmoji(this)" value="{{ asset('IMAGES/EMOJI/BUTTERFLY.PNG') }}"/>
@@ -708,6 +708,7 @@
 
                 ChangePosition();
             }
+            $('#custom-text').focus();
         }
 
         function ChangePosition(){
@@ -823,71 +824,147 @@
                     var posXimg = Number(posX);
                     for(var l=0; l<srcList.length;l++) {
                         if(srcList[l] !== "" ){
-                            if(srcList[l].indexOf(".PNG") >= 0){
-                                if(l === 0){
-                                    posXimg = posXimg - Number(30);
-                                    newText = newText + "     ";
-                                }
-                                else if(l === 1){
-                                    //cek jika gambar pertama
-                                    if(ct === 0){
-                                        posXimg = posXimg - Number(15);
-                                        newText = newText + "    ";
+                            // mobile view
+                            if(isMobile){
+                                if(srcList[l].indexOf(".PNG") >= 0){
+                                    if(l === 0){
+                                        posXimg = posXimg - Number(30);
+                                        newText = newText + "   ";
+                                    }
+                                    else if(l === 1){
+                                        //cek jika gambar pertama
+                                        if(ct === 0){
+                                            posXimg = posXimg - Number(8);
+                                            newText = newText + "   ";
+                                        }
+                                        else{
+                                            posXimg = posXimg + Number(16);
+                                            newText = newText + "     ";
+                                        }
+                                    }
+                                    else if(l === 2){
+                                        //cek jika gambar pertama
+                                        if(ct === 0){
+                                            posXimg = posXimg - Number(5);
+                                            newText = newText + "   ";
+                                        }
+                                        else{
+                                            posXimg = posXimg + Number(16);
+                                            newText = newText + "      ";
+                                        }
+                                    }
+                                    else if(l === 3){
+                                        //cek jika gambar pertama
+                                        if(ct === 0){
+                                            posXimg = posXimg - Number(2);
+                                            newText = newText + "   ";
+                                        }
+                                        else{
+                                            posXimg = posXimg + Number(11);
+                                            newText = newText + "    ";
+                                        }
                                     }
                                     else{
-                                        posXimg = posXimg + Number(15);
-                                        newText = newText + "     ";
+                                        if(ct === 0){
+                                            posXimg = posXimg + Number(9);
+                                        }
+                                        else{
+                                            posXimg = posXimg + Number(11);
+                                            newText = newText + "   ";
+                                        }
                                     }
-                                }
-                                else if(l === 2){
-                                    //cek jika gambar pertama
-                                    if(ct === 0){
-                                        posXimg = posXimg - Number(5);
-                                        newText = newText + "    ";
-                                    }
-                                    else{
-                                        posXimg = posXimg + Number(16);
-                                        newText = newText + "     ";
-                                    }
-                                }
-                                else if(l === 3){
-                                    //cek jika gambar pertama
-                                    if(ct === 0){
-                                        posXimg = posXimg - Number(2);
-                                        newText = newText + "    ";
-                                    }
-                                    else{
-                                        posXimg = posXimg + Number(14);
-                                        newText = newText + "     ";
-                                    }
+                                    var posYimg = posY - 13;
+                                    context.drawImage(imgs[ct], posXimg, posYimg, 14, 14);
+                                    ct++;
                                 }
                                 else{
-                                    if(ct === 0){
-                                        posXimg = posXimg + Number(9);
+                                    // newText = newText + srcList[l];
+                                    // posXimg = posXimg + Number(8);
+                                    newText = newText + srcList[l];
+                                    if(l <= 1){
+                                        if(ct !== 0){
+                                            posXimg = posXimg + Number(9);
+                                        }
                                     }
                                     else{
-                                        posXimg = posXimg + Number(17);
+                                        if(ct !== 0){
+                                            posXimg = posXimg + Number(11);
+                                        }
+                                        else{
+                                            posXimg = posXimg + Number(5);
+                                        }
                                     }
                                 }
-                                var posYimg = posY - 13;
-                                context.drawImage(imgs[ct], posXimg, posYimg, 15, 15);
-                                ct++;
                             }
+                            // desktop view
                             else{
-                                // newText = newText + srcList[l];
-                                // posXimg = posXimg + Number(8);
-                                newText = newText + srcList[l];
-                                if(l <= 1){
-                                    if(ct !== 0){
-                                        posXimg = posXimg + Number(12);
+                                if(srcList[l].indexOf(".PNG") >= 0){
+                                    if(l === 0){
+                                        posXimg = posXimg - Number(35);
+                                        newText = newText + "    ";
                                     }
-                                }
-                                else{
-                                    if(ct !== 0){
-                                        posXimg = posXimg + Number(13);
+                                    else if(l === 1){
+                                        //cek jika gambar pertama
+                                        if(ct === 0){
+                                            posXimg = posXimg - Number(18);
+                                            newText = newText + "    ";
+                                        }
+                                        else{
+                                            posXimg = posXimg + Number(16);
+                                            newText = newText + "      ";
+                                        }
+                                    }
+                                    else if(l === 2){
+                                        //cek jika gambar pertama
+                                        if(ct === 0){
+                                            posXimg = posXimg - Number(9);
+                                            newText = newText + "     ";
+                                        }
+                                        else{
+                                            posXimg = posXimg + Number(14);
+                                            newText = newText + "    ";
+                                        }
+                                    }
+                                    else if(l === 3){
+                                        //cek jika gambar pertama
+                                        if(ct === 0){
+                                            posXimg = posXimg - Number(2);
+                                            newText = newText + "    ";
+                                        }
+                                        else{
+                                            posXimg = posXimg + Number(14);
+                                            newText = newText + "     ";
+                                        }
                                     }
                                     else{
-                                        posXimg = posXimg + Number(5);
+                                        if(ct === 0){
+                                            posXimg = posXimg + Number(9);
+                                        }
+                                        else{
+                                            posXimg = posXimg + Number(15);
+                                            newText = newText + "    ";
+                                        }
+                                    }
+                                    var posYimg = posY - 13;
+                                    context.drawImage(imgs[ct], posXimg, posYimg, 15, 15);
+                                    ct++;
+                                }
+                                else{
+                                    // newText = newText + srcList[l];
+                                    // posXimg = posXimg + Number(8);
+                                    newText = newText + srcList[l];
+                                    if(l <= 1){
+                                        if(ct !== 0){
+                                            posXimg = posXimg + Number(12);
+                                        }
+                                    }
+                                    else{
+                                        if(ct !== 0){
+                                            posXimg = posXimg + Number(13);
+                                        }
+                                        else{
+                                            posXimg = posXimg + Number(5);
+                                        }
                                     }
                                 }
                             }
